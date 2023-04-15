b42e18f5ef35a07e31a0aa877b9207635ada9587
ryaev <robert.yaev@gmail.com>
2023-04-15 18:07:49 -0300

    BaSP-W05-2023: strings exercises

---
+++ Week-05/exercises/strings.js
@@ -0,0 +1,74 @@
+console.log('--EXERCISE 2: STRINGS');
+
+/*  a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en 
+    mayúscula (utilizar toUpperCase).   */
+
+console.log('-Exercise 2.a:');
+
+var stringA= 'Montevideo';
+console.log(stringA.toUpperCase());
+
+/*  b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
+    los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).    */
+
+console.log('-Exercise 2.b:');
+
+var stringB= 'Montevideo';
+var stringFirstFive = stringB.substring(0, 5);
+console.log(stringFirstFive);
+
+/*  c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
+    los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).    */
+
+console.log('-Exercise 2.c:');
+
+var stringC= 'Montevideo';
+var stringLastThree = stringC.substring(stringC.length-3);
+console.log(stringLastThree);
+
+/*  d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
+    la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva 
+    variable (utilizar substring, toUpperCase, toLowerCase y el operador +).    */
+
+console.log('-Exercise 2.d:');
+
+var stringD= 'wAsHiNgToN';
+var stringFirstLetter= stringD.substring(0, 1);
+var stringFirstUpper= stringFirstLetter.toUpperCase();
+var stringRestLetters= stringD.substring(1,stringD.length);
+var stringRestLower= stringRestLetters.toLowerCase();
+var stringDResult = (stringFirstUpper + stringRestLower);
+console.log(stringDResult);
+
+/*  e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
+    Encontrar la posición del primer espacio en blanco y guardarla en una variable 
+    (utilizar indexOf).    */
+
+console.log('-Exercise 2.e:');
+
+var stringE= 'Planet earth';
+var firstSpace= stringE.indexOf(' ');
+console.log(firstSpace);
+
+/*  f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún 
+    espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un 
+    nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras 
+    en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).    */
+
+console.log('-Exercise 2.f:');
+
+var stringF= 'mOnTeViDeO wAsHiNgToN';
+var firstSpaceString= stringF.indexOf(' ');
+var beginSecondWord = firstSpaceString + 1;
+var firstWordLetter= stringF.substring(0, 1);
+var firstWordUpper= firstWordLetter.toUpperCase();
+var firstWordRest= stringF.substring(1, firstSpaceString);
+var firstWordRestLower= firstWordRest.toLowerCase();
+var firstWordResult = (firstWordUpper + firstWordRestLower);
+var secondWordLetter= stringF.substring(beginSecondWord, (beginSecondWord + 1));
+var secondWordUpper= secondWordLetter.toUpperCase();
+var secondWordRest= stringF.substring((beginSecondWord + 1), stringF.length);
+var secondWordRestLower= secondWordRest.toLowerCase();
+var secondWordResult= (secondWordUpper + secondWordRestLower);
+var stringFResult= (firstWordResult + ' ' + secondWordResult)
+console.log(stringFResult);
--- Week-05/exercises/variables_and_operators.js
+++ Week-05/exercises/variables_and_operators.js
@@ -1,9 +1,9 @@
-console.log("--EXERCISE 1: VARIABLES AND OPERATORS");
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
 
 /*  a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos 
     números en una 3er variable.    */
 
-console.log("-Exercise 1.a:");
+console.log('-Exercise 1.a:');
 
 var number1= 30;
 var number2= 25;
@@ -12,19 +12,19 @@ console.log(sum);
 
 /*  b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.   */
 
-console.log("-Exercise 1.b:");
+console.log('-Exercise 1.b:');
 
-var capital= "Montevideo";
-var country= "Uruguay";
-var capital_country= (capital + ", " + country);
-console.log(capital_country);
+var capital= 'Montevideo';
+var country= 'Uruguay';
+var capitalCountry= (capital + ', ' + country);
+console.log(capitalCountry);
 
-/*  Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+/*  c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
     guardando el resultado de la suma en una 3er variable (utilizar length).    */
 
-console.log("-Exercise 1.c:");
+console.log('-Exercise 1.c:');
 
-var name_person= "Robert";
-var last_name= "Yanez";
-var length_full_name= name_person.length + last_name.length;
-console.log(length_full_name);
+var namePerson= 'Robert';
+var lastName= 'Yanez';
+var lengthFullName= namePerson.length + lastName.length;
+console.log(lengthFullName);
